fbe65d7f6bc3b47161b10a2770a43f4dbb0bd56f
vikasslandge <vikasslandge@gmail.com>
2018-07-15 20:39:26 +0530

    initial

--- app/src/main/java/com/example/vikaslandge/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/vikaslandge/firstapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.vikaslandge.firstapp
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    b1.setOnClickListener(){
+
+        tv1.text =et1.text
     }
+
+    }
+
 }
